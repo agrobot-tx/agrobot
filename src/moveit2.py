4150c4b31bc198ffad19242809f14e7dab8da6cb
NaomideVos <47396469+NaomideVos@users.noreply.github.com>
2021-02-04 16:53:30 +0100

    Update moveit2.py

--- src/moveit2.py
+++ src/moveit2.py
@@ -20,13 +20,11 @@ group.clear_pose_targets()
 # group.forget_joint_values()
 # group.get_current_joint_values()
 
+# Setting only a goal for the lowest joint
 group_variable_values = group.get_current_joint_values()
 print "============ Joint values: ", group_variable_values
-
 group_variable_values[0] = 1.0
 group.set_joint_value_target(group_variable_values)
-
-plan2 = group.plan()
 
 rospy.sleep(5)
 

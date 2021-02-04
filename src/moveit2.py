77b51001635c0d99d9fd88545d8678aebdcd1762
NaomideVos <47396469+NaomideVos@users.noreply.github.com>
2021-02-04 16:52:18 +0100

    Create moveit2.py

---
+++ src/moveit2.py
@@ -0,0 +1,38 @@
+import sys
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('my_moveit_group', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group = moveit_commander.MoveGroupCommander("arm")
+
+# clear constraints
+group.clear_path_constraints()
+group.clear_trajectory_constraints()
+group.clear_pose_targets()
+
+# reset joint values
+# group.forget_joint_values()
+# group.get_current_joint_values()
+
+group_variable_values = group.get_current_joint_values()
+print "============ Joint values: ", group_variable_values
+
+group_variable_values[0] = 1.0
+group.set_joint_value_target(group_variable_values)
+
+plan2 = group.plan()
+
+rospy.sleep(5)
+
+# It is always good to clear your targets after planning with poses.
+group.clear_pose_targets()
+
+rospy.sleep(5)
+
+moveit_commander.roscpp_shutdown()
